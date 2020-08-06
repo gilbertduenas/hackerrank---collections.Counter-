8c89a53710673866223373f2b78d882fb1829a90
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-06 22:24:12 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,14 @@
-from collections import Counter
+# brute force code, no from collections import Counter
+
+num_shoes = int(input())
+shoe_sizes = list(map(int, input().split()))
+num_cust = int(input())
+total = 0
+
+for i in range(num_cust):
+    order = list(map(int, input().split()))
+    if order[0] in shoe_sizes:
+        total += order[1]
+        shoe_sizes.remove(order[0])
+
+print(total)
